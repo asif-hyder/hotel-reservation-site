6c56bed861b82a0b9f9bb3b787465fb874e0051e
Asif <asif.turabi512@gmail.com>
2023-08-09 07:16:04 -0700

    init the project

---
+++ src/assets/index.js
@@ -0,0 +1,35 @@
+import room1 from "./images/details-1.jpeg";
+import room2 from "./images/details-2.jpeg";
+import room3 from "./images/details-3.jpeg";
+import room4 from "./images/details-4.jpeg";
+import img1 from "./images/room-1.jpeg";
+import img2 from "./images/room-2.jpeg";
+import img3 from "./images/room-3.jpeg";
+import img4 from "./images/room-4.jpeg";
+import img5 from "./images/room-5.jpeg";
+import img6 from "./images/room-6.jpeg";
+import img7 from "./images/room-7.jpeg";
+import img8 from "./images/room-8.jpeg";
+import img9 from "./images/room-9.jpeg";
+import img10 from "./images/room-10.jpeg";
+import img11 from "./images/room-11.jpeg";
+import img12 from "./images/room-12.jpeg";
+
+export const images = {
+    room1: room1,
+    room2: room2,
+    room3: room3,
+    room4: room3,
+    img1: img1,
+    img2: img2,
+    img3: img3,
+    img4: img4,
+    img5: img5,
+    img6: img6,
+    img7: img7,
+    img8: img8,
+    img9: img9,
+    img10: img10,
+    img11: img11,
+    img12: img12
+}
